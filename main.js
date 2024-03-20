aecbb8026b44a049f249896534f354826989ce77
Dorian Poleni <dpoleni@yahoo.it>
2024-03-20 23:29:31 +0100

    inserted time counter function

--- main.js
+++ main.js
@@ -9,36 +9,29 @@ nComments = document.querySelector("#nComments");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 200){
-     navbar.classList.add("navbarScrolled");
-     navbar.classList.add("transitionStandard");
-    //  navbarBrand.classList.remove("navbar-brand");
-    //  navbarBrand.classList.add("brandScrolled");
-
-    //  TEXTToLogo
-    //  navbarBrand.innerHTML ="";
-    // containerNav.innerHTML = `<img src="./media/logoipsum-212.svg" alt="">`
-    // containerNav.appendChild(logo); 
-
-
+        navbar.classList.add("navbarScrolled");
+        navbar.classList.add("transitionStandard");
+        //  TEXTToLogo
+        navbarBrand.innerHTML ="";
+        navbarBrand.innerHTML = `<img src="./media/logoipsum-212.svg" alt="">`    
     }
     else{   
         //LogotoTEXT
-        // containerNav.innerHTML ="";
-        // containerNav.innerHTML = ` <a class="navbar-brand" id="navbarBrand">Presto.it</a>`
-        // containerNav.appendChild(logo); 
-
+        navbarBrand.innerHTML ="";
+        navbarBrand.innerHTML = `Presto.it`
+        
         navbar.classList.remove("navbarScrolled");
-        navbarBrand.classList.remove("brandScrolled");
+        // navbarBrand.classList.remove("brandScrolled");
         navbar.classList.add("transitionStandard");
     }
-
-
+    
+    
 })
 
 
-counterTimer(1000,1,nArticles);
-counterTimer(500,10,nUsers);
-counterTimer(200,20,nComments);
+counterTimer(10,100,nArticles);
+counterTimer(500,100,nUsers);
+counterTimer(300,100,nComments);
 
 
 
@@ -46,7 +39,7 @@ counterTimer(200,20,nComments);
 //Function Declaration
 function counterTimer(setPoint, time, target){
     let counter = 0;
-
+    
     let interval = setInterval(() => {
         if (counter < setPoint){
             counter++;
@@ -56,5 +49,5 @@ function counterTimer(setPoint, time, target){
         else {
             clearInterval(interval);
         }
-    }, time);
+    }, (time/setPoint)*time);
 }
