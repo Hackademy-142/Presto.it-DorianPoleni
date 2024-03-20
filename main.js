959d7a28689a47b6e77acb5409281cfe9d0f17c4
Dorian Poleni <dpoleni@yahoo.it>
2024-03-20 22:35:57 +0100

    completed asinchronous functions and number counters

--- main.js
+++ main.js
@@ -3,9 +3,11 @@
 navbar = document.querySelector(".navbar");
 navbarBrand = document.querySelector("#navbarBrand");
 containerNav = document.querySelector("#containerNav");
+nArticles = document.querySelector("#nArticles");
+nUsers = document.querySelector("#nUsers");
+nComments = document.querySelector("#nComments");
 
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
     if (window.scrollY > 200){
      navbar.classList.add("navbarScrolled");
      navbar.classList.add("transitionStandard");
@@ -32,3 +34,27 @@ window.addEventListener("scroll", () => {
 
 
 })
+
+
+counterTimer(1000,1,nArticles);
+counterTimer(500,10,nUsers);
+counterTimer(200,20,nComments);
+
+
+
+
+//Function Declaration
+function counterTimer(setPoint, time, target){
+    let counter = 0;
+
+    let interval = setInterval(() => {
+        if (counter < setPoint){
+            counter++;
+            console.log(counter);
+            target.innerHTML = counter;
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, time);
+}
